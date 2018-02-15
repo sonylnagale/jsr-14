b0396daaf3ac12c4ee8fb33887530fdc8020c47e
Mark <mdkoenig@gmail.com>
2018-02-14 23:38:38 -0500

    Got the variable animals and the moving animals -- can't figure out talk

--- app.js
+++ app.js
@@ -3,26 +3,9 @@ $('body').on('click', function() {
     var x = event.clientX + "px";
     var y = event.clientY + "px";
     console.log("X: " + x + "&" + "Y: " + y);
-    var randoAnimal = Math.floor(Math.random()*5);
-    //console.log(randoAnimal);
-    placeAnimal(x,y,randoAnimal);
-    document.getElementById("falcon").style.top = y;
-    document.getElementById("falcon").style.left = x;
+    $("#" + currentAnimal).css({"top":y,"left":x});
 });
 
-function placeAnimal(x,y,randoAnimal) {
-
-}
-
-var falcons = 0;
-var sparrows = 0;
-var pigs = 0;
-var unicorns = 0;
-var hippos = 0;
-
-// var animals = ["falcon","sparrpw","pig","unicorn","hippo"];
-
-var animals = [];
 
 function FarmAnimal(name, image, sound, x, y) {
 	this.name = name;
@@ -33,50 +16,46 @@ function FarmAnimal(name, image, sound, x, y) {
 	type = "animal";
 }
 
-//var sparrow = new FarmAnimal("sparrow","http://pm1.narvii.com/6387/fd94cb88fa3f4008997a6074167122810f4330a0_128.jpg","twitter twitter");
+var animalOptions = [];
 
-//var pig = new FarmAnimal("pig","http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-11/256/pig-face.png","oink oink");
+var sparrow = new FarmAnimal("sparrow","http://www.ikalta.com/ga/farm/images/sparrow.gif","twitter twitter");
+animalOptions.push(sparrow);
 
-//var unicorn = new FarmAnimal("unicorn","https://a.ppy.sh/3610482_1452346309.png","friendship is magic");
+var pig = new FarmAnimal("pig","http://www.ikalta.com/ga/farm/images/pig.gif","oink oink");
+animalOptions.push(pig);
 
-///var hippo = new FarmAnimal("hippo","https://at-cdn-s01.audiotool.com/2011/05/15/documents/yQKhubIcrklLfmA0l04Ct3xKz2hvASQ0/0/cover256x256-e15b699236554e6389799c858b33932d.jpg","death");
+var unicorn = new FarmAnimal("unicorn","http://www.ikalta.com/ga/farm/images/unicorn.gif","friendship is magic");
+animalOptions.push(unicorn);
+
+var hippo = new FarmAnimal("hippo","http://www.ikalta.com/ga/farm/images/hippo.gif","death");
+animalOptions.push(hippo);
 
 var falcon = new FarmAnimal("falcon","http://www.ikalta.com/ga/farm/images/falcon.gif","screeech");
+animalOptions.push(falcon);
 
-var $newAnimal = $("<span id='" + falcon.name + "' class='animal'><img src='" + falcon.image + "'></span>");
-
-animals.push($newAnimal);
-
-//falcon.talk();
-
+ 
 $(document).ready(function () {
-	$("#falcon").click(function() {
-		falcon.talk()
+	$(".animal").click(function() {
+		animal = $(".animal").attr("id");
+		console.log(animal);
+		eval(animal).talk()
 	})
 });
 
-$(".container-farm").append(animals[0]);
-
-//document.getElementById("falcon").style.top = "100px";
-
-/*
-$('#falcon').on('click', function() {
-    alert(falcon.talk);
+$("#new").click(function() {
+	newAnimal();
 });
-*/
 
-/*
-function Vehicle(color) {
-  this.color = color;
+animalNumber = 0;
+var currentAnimal = "";
+
+function newAnimal() {
+	var randoAnimal = Math.floor(Math.random()*5);
+	//var animal = animalOptions[randoAnimal];
+	var animal = animalOptions[randoAnimal];
+	console.log(animal.name);
+	var $newAnimal = $("<span id='" + animal.name + animalNumber + "' name='" + animal.name + "' class='animal'><img src='" + animal.image + "'></span>");
+	currentAnimal = animal.name+animalNumber;
+	animalNumber += 1;
+	$(".container-farm").append($newAnimal);
 }
-
-function HybridCar(color) {
-  Vehicle.call(this, color);
-}
-
-HybridCar.prototype = new Vehicle();
-
-var prius = new HybridCar('white');
-
-prius.color // "white"
-*/
